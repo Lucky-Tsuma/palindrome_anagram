8090778d423cccc8546301bb02e2e0690cfc53cc
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-11-04 16:42:24 +0300

    Added express

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
 const checkForPalindrome = (text) => {
-  text = text.toLowerCase();
-  const charArray = text.split("");
-  const noSpaces = [];
-  charArray.map((char) => { if(char !== " ") noSpaces.push(char) });
-
-  text = noSpaces.join("");
-  reversed_text = noSpaces.reverse().join("");
-
-  text === reversed_text ? console.log("Palindrome") : console.log("Not a palindrome");
-
-}
-
-checkForPalindrome("Mr owl ate my metal worm"); //Palindrome
-checkForPalindrome("The Jitu"); //Not a palindrome
-
+    text = text.toLowerCase();
+    const charArray = text.split("");
+    const noSpaces = [];
+    charArray.map((char) => { if(char !== " ") noSpaces.push(char) });
+    
+    text = noSpaces.join("");
+    reversed_text = noSpaces.reverse().join("");
+    
+    return text === reversed_text ? "Palindrome" : "Not a palindrome";
+    
+}  
 
 const checkForAnagram = (text_one, text_two) => {
     text_one = text_one.toLowerCase();
     text_two = text_two.toLowerCase();
-
+    
     let charArray1 = text_one.split("").sort();
     let charArray2 = text_two.split("").sort();
     text_one = charArray1.join("");
     text_two = charArray2.join("");
-
-    text_one === text_two ? console.log("Anagrams") : console.log("Not Anagrams");
-
+    
+    return text_one === text_two ? "Anagrams" : "Not Anagrams";
+    
 }
 
-checkForAnagram("Listen", "Silent"); //Anagrams
-checkForAnagram("Fortune", "Fortunate"); //Not Anagrams
+
+app.get('/', (req, res) => {
+    res.send("Running. Homepage");
+});
+
+app.post('/palindrome', (req, res) => {
+    const { text } = req.body;
+    const isPalindrome = checkForPalindrome(text);
+    res.send(isPalindrome);
+});
+
+app.post('/anagram', (req, res) => {
+    const {text_one, text_two} = req.body;
+    const areAnagrams = checkForAnagram(text_one, text_two);
+    res.send(areAnagrams);
+});
+
+app.listen(port, () => {
+    console.log(`Application running on port: ${port}`)
+});
